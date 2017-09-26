9c0acf0c12ace467c2bfe54d59bd9d392c6ebad6
Michael Chiang <michael.chiang25@gmail.com>
2017-09-26 14:22:14 -0400

    Functional app. Added Insturctions to README.md

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # terminal-node-app
-simple cmd line node app
+Simple cmd line node app
+
+### To Setup
+run ```npm install``` 
+...and that's it!
+
+### Basic Command in Terminal/CMD.EXE:
+```
+	node liri.js <arg> <param>
+```
+### Acceptable <args>
+	```
+	my-tweets
+	spotify-this-song <param>
+	movie-this <param>
+	do-what-it-says
+	```
--- liri.js
+++ liri.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+var fs = require('fs');
 var request = require('request');
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
@@ -7,50 +7,74 @@ var Spotify = require('node-spotify-api');
 
 var request = require('request');
 var userCategory = process.argv[2];
-var userArgs = process.argv.split(3).join(" ");
+var userArgs = process.argv.splice(3).join(" ");
 
-console.log(userCategory, userArgs);
+if (userCategory && userArgs) {
+    console.log(`\n Input: ${userCategory} ${userArgs} \n`);
+}
+checker();
 
-
-switch (userCategory) {
-    case "my-tweets":
-        callTwitter();
-        break;
-    case "spotify-this-song":
-        callSpotify();
-        break;
-    case "movie-this":
-        callMovie();
-        break;
-    case "do-what-it-says":
-        callWhat();
-        break;
-    default:
-        console.log(`Please input one of the following: /\n "my-tweets" /\n "spotify-this-song", /\n "movie-this", /\n "do-what-it-says"`);
+function checker() {
+    switch (userCategory) {
+        case "my-tweets":
+            callTwitter();
+            break;
+        case "spotify-this-song":
+            callSpotify();
+            break;
+        case "movie-this":
+            callMovie();
+            break;
+        case "do-what-it-says":
+            callWhat();
+            break;
+        default:
+            console.log(`\nPlease input one of the following: \n \n my-tweets \n spotify-this-song '<song name here>' \n movie-this '<movie name here>' \n do-what-it-says \n\n`);
+    }
 }
 
-//OMDB
+
+//OMDB AJAX CALL
 function callMovie() {
-    var movieName = "";
-
-    movieName = name;
-
+    var movieName = userArgs ? userArgs : "Shawshank Redemption";
+    if (!userArgs) {
+        console.log(`\nMissing parameters detected, defaulting result to Shawshank Redemption... \n`);
+    }
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+    var totalM = "";
 
     request(queryUrl, function(err, res, body) {
         if (err) {
             console.log(err, res);
         }
         if (!err && res.statusCode === 200) {
-            console.log(body);
+            var movieObj = JSON.parse(body);
+
+            var rottenTomatoesRating;
+            var ratings = movieObj.Ratings;
+            for (rating in ratings) {
+                if (ratings[rating].Source === "Rotten Tomatoes") {
+                    rottenTomatoesRating = `Rotten Tomatoes Rating: ${ratings[rating].Value}`;
+                }
+            }
+            totalM = `Movie Title: ${movieObj.Title}
+Year Released: ${movieObj.Year}
+IMDB Rating: ${movieObj.imdbRating}
+${rottenTomatoesRating}
+Location of Production: ${movieObj.Country}
+Language: ${movieObj.Language}
+Synopsis: ${movieObj.Plot}
+Actors: ${movieObj.Actors}`
+
         }
 
-        // console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+        console.log(totalM);
+        fAppend(userCategory, totalM);
     });
 }
 
 
-//TWITTER
+//TWITTER AJAX CALL
 function callTwitter() {
     var client = new Twitter({
         consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -59,20 +83,76 @@ function callTwitter() {
         access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
     });
 
-    var tUrl = "";
-
-    // client.get()
+    client.get('favorites/list', function(err, tweets, res) {
+        if (err) {
+            throw err;
+        }
+        var totalT = "";
+        for (tweet in tweets) {
+            var t = JSON.stringify(tweets[tweet], null, 4);
+            console.log(` ${t} \n`);
+            totalT += `${t} \n`
+        }
+        fAppend(userCategory, totalT)
+    });
 }
 
 
 
 
-//SPOTIFY
+//SPOTIFY AJAX CALL
 function callSpotify() {
-    var sUrl = "";
 
     var spotify = new Spotify({
         id: process.env.SPOTIFY_CLIENT_ID,
         secret: process.env.SPOTIFY_CLIENT_SECRET
+    });
+    var totalS = "";
+
+    if (!userArgs) {
+        console.log(`\nMissing parameters detected, defaulting Result to "The Sign" by Ace of Base... \n`);
+    }
+
+    spotify
+        .search({ type: 'track', query: userArgs ? userArgs : "The Sign ace of base" })
+        .then(function(res) {
+            spotifyObj = res.tracks.items[0];
+            // console.log(spotifyObj);
+            var artists = spotifyObj.artists;
+            var artistsList = [];
+            for (artist in artists) {
+                artistsList.push(artists[artist].name)
+            }
+            totalS = `Artist(s): ${artistsList.join(", ")}
+Song Name: ${spotifyObj.name}
+Preview the song here: ${spotifyObj.preview_url}
+Album Name: ${spotifyObj.album.name}\n`;
+          
+            console.log(totalS);
+            fAppend(userCategory, totalS)
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+}
+
+function callWhat() {
+    fs.readFile('./random.txt', 'utf8', (err, data) => {
+        if (err) throw err;
+        // console.log(data);
+        fileArray = data.split(",");
+        // console.log(fileArray);
+        userCategory = fileArray[0];
+        userArgs = fileArray[1];
+        checker();
+    });
+}
+
+
+function fAppend(input, result) {
+    // console.log(input);
+    fs.appendFile('./log.txt', `\ncmd=${input}\n\n${result}\n`, 'utf8', (err, data) => {
+        if (err) throw err;
+        console.log(`\nSuccessfully appended data to file.`)
     });
 }
